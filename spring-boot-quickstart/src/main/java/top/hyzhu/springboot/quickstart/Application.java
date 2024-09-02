9bd7b5a38a01cc9d22f0254a23e44c03d79574a0
hyzhu <zhuhuiyu2499669698@163.com>
2024-09-02 15:48:59 +0800

    Spring Boot 课程学习多模块项目初始化

---
+++ spring-boot-quickstart/src/main/java/top/hyzhu/springboot/quickstart/Application.java
@@ -0,0 +1,16 @@
+package top.hyzhu.springboot.quickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: zhy
+ * @Description: Application
+ * @Date: 2024-09-02 15:46
+ **/
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
