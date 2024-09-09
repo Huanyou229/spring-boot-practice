650fbfe6e14cd86c06d759b2c800e7402a56a9c5
hyzhu <zhuhuiyu2499669698@163.com>
2024-09-09 19:46:30 +0800

    日志管理的学习

---
+++ spring-boot-logging/src/main/java/top/hyzhu/springboot/logging/Application.java
@@ -0,0 +1,16 @@
+package top.hyzhu.springboot.logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: zhy
+ * @Description: 启动类
+ * @Date: 2024-09-09 19:21
+ **/
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
